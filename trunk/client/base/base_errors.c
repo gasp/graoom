fed5304b364f68d9d1b3b59de91330fbc4d7ca02
rannou.sebastien <rannou.sebastien@7317104c-667d-11de-9b35-9190732fe600>
2009-07-20 16:35:11 +0000

    added way to log errors in a similar way of the server

---
+++ trunk/client/base/base_errors.c
@@ -0,0 +1,225 @@
+/*
+** errors.c for graoom in /home/rannou_s/Work/graoom/server
+** 
+** Made by sebastien rannou
+** Login   <rannou_s@epitech.net>
+** 
+** Started on  Wed Jul  8 22:23:24 2009 sebastien rannou
+** Last update Mon Jul 20 18:32:15 2009 sebastien rannou
+*/
+
+#include "errors.h"
+#include "shortcuts.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**!
+ * @author	rannou_s
+ * This file contains errors informations allowing them to have a different
+ * behavior when raised. Each error has a code and an additionnal string 
+ * describing it. There's also an additional flag which will determine 
+ * what we should do when raised, this flag can combine:
+ *	- ERR_T_DISPLAY	: outputs string on stderr
+ *	- ERR_T_LOG	: log it to a file
+ *	- ERR_T_DIE	: leave the program
+ */
+
+typedef struct	error_s		/* structure describing an error */
+{
+  int		code;		/* associated code */
+  char		*fmt;		/* associated message */
+  char		behavior;	/* bytes to know what to do when raised */
+}		error_t;
+
+static
+error_t		global_errors[] =
+  {
+
+    /* NULL pointer found (expected) */
+    {
+      .code	=	EC_NULL_PTR,
+      .fmt	=	"NULL pointer",
+      .behavior	=	ERR_T_DISPLAY | ERR_T_LOG
+    },
+
+    /* NULL pointer found (unexpected) */
+    {
+      .code	=	EC_NULL_PTR_DIE,
+      .fmt	=	"NULL pointer (critical)",
+      .behavior	=	ERR_T_DISPLAY | ERR_T_LOG | ERR_T_DIE
+    },
+
+    /* malloc syscall failed */
+    {
+      .code	=	EC_SYS_MALLOC,
+      .fmt	=	"unable to perform malloc (%s)",
+      .behavior	=	ERR_T_LOG
+    },
+
+    /* Unable to load ini file */
+    {
+      .code	=	EC_INI_FILE,
+      .fmt	=	"unable to load ini file (%s)", 
+      .behavior	=	ERR_T_DISPLAY | ERR_T_LOG
+    },
+
+    /* Unable to find section in ini file */
+    {
+      .code	=	EC_INI_SEC,
+      .fmt	=	"unable to find section (%s) in ini file (%s)", 
+      .behavior	=	ERR_T_DISPLAY | ERR_T_LOG
+    },
+
+    /* Unknown entry in ini file */
+    {
+      .code	=	EC_INI_UNKNOWN_ENTRY,
+      .fmt	=	"unknown entry (%s) in ini file (%s)", 
+      .behavior	=	ERR_T_DISPLAY | ERR_T_LOG
+    },
+
+    /* End of array */
+    {
+      .code	=	0x0,
+      .fmt	=	NULL,
+      .behavior	=	0
+    }
+
+  };
+
+static
+FILE		*global_error_log = NULL;
+
+/**!
+ * @author	rannou_s
+ * called at exit to close global_error_log
+ */
+
+void
+error_handler_log_close(void)
+{
+  fclose(global_error_log);
+}
+
+/**!
+ * @author	rannou_s
+ * Initialize error log file by creating it, do not
+ * log if we can't clean it at exit
+ */
+
+static __inline void
+error_handler_log_init(void)
+{
+  global_error_log = fopen(ERR_LOG_FILE, FOP_AP);
+  if (global_error_log != NULL)
+    {
+      if (atexit(&error_handler_log_close) != 0)
+	{
+	  fclose(global_error_log);  
+	}
+    }
+}
+
+/**!
+ * @author	rannou_s
+ * called when an error is raised and when ERR_T_LOG flag is set
+ * We assume ap contains list of arguments to vfprintf and we use it
+ * to generate a nice log
+ * First time a log is executed, initialize the error log file
+ */
+
+#define	ERROR_BUFF_SIZE	512
+#define	ERROR_FMT_LOG	"error raised in [%s] on line %d: "
+
+static __inline void
+error_handler_log(error_t *err, int line, char *file, va_list ap)
+{
+  static int	initialized = 0;
+  char		*date;
+  char		buffer[ERROR_BUFF_SIZE];
+  time_t	timer;
+  int		len;
+
+  if (err == NULL)
+    return;
+  if (initialized++ == 0)
+    error_handler_log_init();
+  if (global_error_log != NULL)
+    {
+      if ((timer = time(NULL)) > 0)
+	{
+	  date = ctime(&timer);
+	  if (date != NULL && (len = strlen(date)) > 0)
+	    {
+	      date[len - 1] = '\0';
+	      snprintf(buffer, ERROR_BUFF_SIZE, "[%s] ", date);
+	      fwrite(buffer, strlen(buffer), 1, global_error_log);
+	    }
+	}
+      snprintf(buffer, ERROR_BUFF_SIZE, ERROR_FMT_LOG, file, line);
+      fwrite(buffer, strlen(buffer), 1, global_error_log);
+      vsnprintf(buffer, ERROR_BUFF_SIZE, err->fmt, ap);
+      fwrite(buffer, strlen(buffer), 1, global_error_log);
+      fwrite("\n", 1, 1, global_error_log);
+    }
+}
+
+/**!
+ * @author	rannou_s
+ * called when an error is raised and when ERR_T_DISPLAY flag is set
+ * We assume ap contains list of arguments to vfprintf
+ */
+
+static __inline void
+error_handler_display(error_t *err, int line, char *file, va_list ap)
+{
+  if (err != NULL)
+    {
+      fprintf(stderr, ERROR_FMT_LOG, file, line);
+      vfprintf(stderr, err->fmt, ap);
+      fprintf(stderr, "\n");
+      fflush(stderr);
+    }
+}
+
+/**!
+ * @author	rannou_s
+ * Main entry on errors, this is called through and overloading
+ * macro so as to get line and file where error occurs
+ * @todo	- in non-dev mode, it would be great to disable that macros
+ *		- do not va_copy when log is not set
+ */
+
+void
+error_handler(int line, char *file, int code, ...)
+{
+  error_t	*err;
+  va_list	aplog;
+  va_list	ap;
+  int		i;
+
+  va_start(ap, code);
+  va_copy(aplog, ap);
+  for (i = 0; global_errors[i].code != 0x0; i++)
+    {
+      if (global_errors[i].code == code)
+	{
+	  err = &global_errors[i];
+	  if (err->behavior & ERR_T_LOG)
+	    error_handler_log(err, line, file, aplog);
+	  if (err->behavior & ERR_T_DISPLAY)
+	    error_handler_display(err, line, file, ap);
+	  if (err->behavior & ERR_T_DIE)
+	    {
+	      va_end(ap);
+	      va_end(aplog);
+	      exit(ERROR);
+	    }	  
+	}
+    }
+  va_end(aplog);
+  va_end(ap);
+}
---
+++ trunk/client/includes/errors.h
@@ -0,0 +1,55 @@
+/*
+** errors.h for graoom in /home/rannou_s/Work/graoom/server
+** 
+** Made by sebastien rannou
+** Login   <rannou_s@epitech.net>
+** 
+** Started on  Wed Jul  8 20:27:27 2009 sebastien rannou
+** Last update Mon Jul 20 18:32:46 2009 sebastien rannou
+*/
+
+#ifndef GRAOOM_C_ERRORS_H
+#define	GRAOOM_C_ERRORS_H
+
+/**!
+ * @author	rannou_s
+ * error file where logs are written
+ */
+
+#define	ERR_LOG_FILE	"error.log"
+
+/**!
+ * @author	rannou_s
+ * flags related to type_b
+ */
+
+#define			ERR_T_DISPLAY		0x01	/* 0000 0001 */
+#define			ERR_T_LOG		0x02	/* 0000 0010 */
+#define			ERR_T_DIE		0x04	/* 0000 0100 */
+
+/**!
+ * @author	rannou_s
+ * error codes defines
+ */
+
+#define			EC_NULL_PTR		1
+#define			EC_NULL_PTR_DIE		2
+
+#define			EC_SYS_MALLOC		3
+
+#define			EC_INI_FILE		101
+#define			EC_INI_SEC		102
+#define			EC_INI_UNKNOWN_ENTRY	103
+
+void	/* logs a message, see errors.c for more information */
+error_handler(int line, char *file, int code, ...);
+
+/**!
+ * @author	rannou_s
+ * Maccro to make easier tracing of errors in development mode
+ * oh my god we can do varargs inside maccros
+ */
+
+#define	ERR_RAISE(...)	error_handler(__LINE__, __FILE__, __VA_ARGS__)
+
+#endif /* GRAOOM_ERRORS_H */
